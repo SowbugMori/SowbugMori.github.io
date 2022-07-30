53a11de7c378defc6c4d8e6b23314e603ec326e7
Sowbug <58609369+SowbugMori@users.noreply.github.com>
2022-07-30 16:06:54 -0500

    Update challenges.py

--- py/challenges.py
+++ py/challenges.py
@@ -1,6 +1,6 @@
 Challenge = {
-    'Devious Lick': 'Take 7 unique items from the location.',
-    'Lights Out': 'Complete the hunt without turning on the lights. (Ghost light switching can be ignored.)',
+    'Devious Lick': 'Each member must take 7 unique items from the location. (duplicates between members is allowed)',
+    'Lights Out': 'Complete the investigation without turning on the lights. (Ghost light switching can be ignored.)',
     'The Irate Investigator': 'Immediately interact with the cursed object when found until a cursed hunt begins.',
     'Autopilot Disengaged': 'Do not take photos, pick up bones, interact with cursed objects, or collect keys.',
     'Maid Dress Monday': 'Return all equipment brought in to the house.',
